495cd64f1a440594139ccb3d1377b1a4fa6645c8
Jiajun <eric1986_chn@yahoo.com>
2023-08-17 14:46:29 +0200

    Imporve error handling of active_channels

--- src/nebula/nebula.rs
+++ src/nebula/nebula.rs
@@ -114,107 +114,66 @@ pub async fn active_channels(_base_url: &Url) -> Result<HashMap<String, i16>, St
 
     println!("Finding channel routing ...");
 
-    match fetch_spaces(&_base_url).await {
-        Ok(spaces) => {
-            let fst = spaces.first().unwrap();
-            // Handle active_preset_id being null
-            if fst.active_preset_id.is_empty() {
-                println!("Active preset id is empty");
-                return Err("active preset is empty".to_string());
-            }
-            preset = fst.active_preset_id.clone();
-        }
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
 
-    match fetch_preset_id(&_base_url, &preset).await {
-        Ok(preset) => {
-            beam_groups_ids = preset.beam_group_ids;
-            println!("Active preset name: {}", preset.name);
-        }
-        Err(e) => {
-            println!("Error: {}", e);
-        }
+    let spaces = fetch_spaces(&_base_url).await.map_err(|e| e.to_string())?;
+    let fst = spaces.first().unwrap();
+    // Handle active_preset_id being null
+    if fst.active_preset_id.is_empty() {
+        println!("Active preset id is empty");
+        return Err("active preset is empty".to_string());
     }
+    preset = fst.active_preset_id.clone();
+
+    let preset = fetch_preset_id(&_base_url, &preset).await.map_err(|e| e.to_string())?;
+    beam_groups_ids = preset.beam_group_ids;
+    println!("Active preset name: {}", preset.name);
 
     let mut beam_instances_ids: Vec<String> = Vec::new();
 
-    match fetch_beam_groups(&_base_url).await {
-        Ok(beam_groups) => {
-            for bg in beam_groups {
-                if beam_groups_ids.contains(&bg.id) {
-                    beam_instances_ids.extend(bg.beam_instance_ids);
-                }
-            }
-        }
-        Err(e) => {
-            println!("Error: {}", e);
+    let beam_groups = fetch_beam_groups(&_base_url).await.map_err(|e| e.to_string())?;
+    for bg in beam_groups {
+        if beam_groups_ids.contains(&bg.id) {
+            beam_instances_ids.extend(bg.beam_instance_ids);
         }
     }
 
     let mut beam_ids: Vec<String> = Vec::new();
 
-    match fetch_beam_instances(&_base_url).await {
-        Ok(beam_instances) => {
-            for bi in beam_instances {
-                if beam_instances_ids.contains(&bi.id) {
-                    beam_ids.push(bi.beam_id);
-                }
-            }
-        }
-        Err(e) => {
-            println!("Error: {}", e);
+    let beam_instances = fetch_beam_instances(&_base_url).await.map_err(|e| e.to_string())?;
+    for bi in beam_instances {
+        if beam_instances_ids.contains(&bi.id) {
+            beam_ids.push(bi.beam_id);
         }
     }
 
     let mut audio_input_ids: Vec<String> = Vec::new();
 
-    match fetch_beam_audio_inputs(&_base_url).await {
-        Ok(beam_audio_inputs) => {
-            for bai in beam_audio_inputs {
-                if beam_instances_ids.contains(&bai.beam_instance_id) {
-                    if !audio_input_ids.contains(&bai.audio_input_id) {
-                        audio_input_ids.push(bai.audio_input_id);
-                    }
-                }
+    let beam_audio_inputs = fetch_beam_audio_inputs(&_base_url).await.map_err(|e| e.to_string())?;
+    for bai in beam_audio_inputs {
+        if beam_instances_ids.contains(&bai.beam_instance_id) {
+            if !audio_input_ids.contains(&bai.audio_input_id) {
+                audio_input_ids.push(bai.audio_input_id);
             }
-        }
-        Err(e) => {
-            println!("Error: {}", e);
         }
     }
 
     let mut active_audio_input_ids: Vec<String> = Vec::new();
     let mut active_audio_input_map: HashMap<String, String> = HashMap::new();
 
-    match fetch_audio_inputs(&_base_url).await {
-        Ok(audio_inputs) => {
-            for ai in audio_inputs {
-                if audio_input_ids.contains(&ai.id) {
-                    active_audio_input_ids.push(ai.id.clone());
-                    active_audio_input_map.insert(ai.id, ai.name.clone());
-                }
-            }
-        }
-        Err(e) => {
-            println!("Error: {}", e);
+    let audio_inputs = fetch_audio_inputs(&_base_url).await.map_err(|e| e.to_string())?;
+    for ai in audio_inputs {
+        if audio_input_ids.contains(&ai.id) {
+            active_audio_input_ids.push(ai.id.clone());
+            active_audio_input_map.insert(ai.id, ai.name.clone());
         }
     }
 
     let mut active_channels: HashMap<String, i16> = HashMap::new();
 
-    match fetch_audio_input_stream_mappings(&_base_url).await {
-        Ok(audio_input_stream_mappings) => {
-            for aism in audio_input_stream_mappings {
-                if active_audio_input_ids.contains(&aism.audio_input_id) {
-                    active_channels.insert(active_audio_input_map.get(&aism.audio_input_id).unwrap().clone(), aism.channel as i16);
-                }
-            }
-        }
-        Err(e) => {
-            println!("Error: {}", e);
+    let audio_input_stream_mappings = fetch_audio_input_stream_mappings(&_base_url).await.map_err(|e| e.to_string())?;  
+    for aism in audio_input_stream_mappings {
+        if active_audio_input_ids.contains(&aism.audio_input_id) {
+            active_channels.insert(active_audio_input_map.get(&aism.audio_input_id).unwrap().clone(), aism.channel as i16);
         }
     }
 
