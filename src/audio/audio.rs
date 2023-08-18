2759f7b6821a7b85c6ad41ef837022c168973595
Jiajun <eric1986_chn@yahoo.com>
2023-08-18 15:15:11 +0200

    Use sr as option, use a config that has the highest channel count.

--- src/audio/audio.rs
+++ src/audio/audio.rs
@@ -1,6 +1,7 @@
 use rodio::cpal::traits::{DeviceTrait, HostTrait};
 use rodio::source::UniformSourceIterator;
 use rodio::source::{Buffered, ChannelVolume};
+use rodio::cpal::SampleRate;
 use rodio::*;
 use std::fs::File;
 use std::io::BufReader;
@@ -19,19 +20,31 @@ pub fn list_host_devices() {
     println!("---- Completed ----");
 }
 
-pub fn get_output_stream(device_name: &str) -> (OutputStream, OutputStreamHandle, u32, u16) {
+pub fn get_output_stream(device_name: &str, sr: u32) -> (OutputStream, OutputStreamHandle, u16) {
     let host = cpal::default_host();
     let devices = host.output_devices().unwrap();
-    let (mut _stream, mut stream_handle) = OutputStream::try_default().unwrap();
     for device in devices {
         let dev: rodio::Device = device.into();
         let dev_name: String = dev.name().unwrap();
         if dev_name == device_name {
             println!("Device found: {}", dev_name);
-            let sr = dev.default_output_config().unwrap().sample_rate().0;
-            let default_channels = dev.default_output_config().unwrap().channels();
-            (_stream, stream_handle) = OutputStream::try_from_device(&dev).unwrap();
-            return (_stream, stream_handle, sr, default_channels);
+            println!("Accquiring config with the max channels.");
+            let supported_configs = dev.supported_output_configs().unwrap();
+            let mut default_config = dev.default_output_config().unwrap();
+            for supported_config in supported_configs {
+                if supported_config.max_sample_rate() < SampleRate(sr) {
+                    continue;
+                } else {
+                    if supported_config.channels() > default_config.channels() {
+                        default_config = supported_config.with_sample_rate(SampleRate(sr));
+                    }
+                }
+            }
+            let max_ch = default_config.channels() as u16;
+
+            let (_stream, stream_handle) = OutputStream::try_from_device_config(&dev, default_config).unwrap();
+
+            return (_stream, stream_handle, max_ch);
         }
     }
     panic!("Device not found");
